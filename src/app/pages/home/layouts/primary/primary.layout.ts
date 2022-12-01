3de244c1cf68bddb49f368c6c362609789e52064
Mezame <mezamesoft@gmail.com>
2022-12-01 17:40:59 -0600

    Change selector

--- src/app/pages/home/layouts/primary/primary.layout.ts
+++ src/app/pages/home/layouts/primary/primary.layout.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-primary',
+  selector: 'app-primary-layout',
   templateUrl: './primary.layout.html',
   styleUrls: ['./primary.layout.sass']
 })
--- src/app/pages/home/layouts/secondary/secondary.layout.ts
+++ src/app/pages/home/layouts/secondary/secondary.layout.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-secondary',
+  selector: 'app-secondary-layout',
   templateUrl: './secondary.layout.html',
   styleUrls: ['./secondary.layout.sass']
 })
