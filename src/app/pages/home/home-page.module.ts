9a75ba7eb4272f78f25d289949834e5dd7051e73
Mezame <mezamesoft@gmail.com>
2022-12-01 17:21:02 -0600

    Add layouts modules

--- src/app/pages/home/home-page.module.ts
+++ src/app/pages/home/home-page.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { HomeRoutingModule } from './home-routing.module';
+import { PrimaryLayoutModule } from './layouts/primary/primary-layout.module';
+import { SecondaryLayoutModule } from './layouts/secondary/secondary-layout.module';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PrimaryLayoutModule,
+    SecondaryLayoutModule,
+  ],
 })
 export class HomePageModule {}
