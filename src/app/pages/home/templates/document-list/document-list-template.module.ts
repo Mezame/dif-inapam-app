caceea0772630858b78ce4048248eb5604440339
Mezame <mezamesoft@gmail.com>
2022-12-01 17:59:29 -0600

    Add exports

--- src/app/pages/home/templates/document-list/document-list-template.module.ts
+++ src/app/pages/home/templates/document-list/document-list-template.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentListTemplate } from './document-list.template';
+import { PrimaryLayoutModule } from '../../layouts/primary/primary-layout.module';
 
 @NgModule({
   declarations: [DocumentListTemplate],
-  imports: [CommonModule],
+  imports: [CommonModule, PrimaryLayoutModule],
+  exports: [DocumentListTemplate],
 })
 export class DocumentListTemplateModule {}
