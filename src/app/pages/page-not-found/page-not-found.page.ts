08bf3d5a93a5a226fcf2adb9ae64602e1b73540f
Mezame <mezamesoft@gmail.com>
2022-12-01 17:40:23 -0600

    Change selector

--- src/app/pages/page-not-found/page-not-found.page.ts
+++ src/app/pages/page-not-found/page-not-found.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page-not-found',
+  selector: 'app-page-not-found-page',
   templateUrl: './page-not-found.page.html',
   styleUrls: ['./page-not-found.page.sass']
 })
