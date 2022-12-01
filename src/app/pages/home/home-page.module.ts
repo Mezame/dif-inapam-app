e0cfa1bdf0f2d6b85e417ff704cd776d0b95a2d4
Mezame <mezamesoft@gmail.com>
2022-12-01 17:55:47 -0600

    Remove layout modules

--- src/app/pages/home/home-page.module.ts
+++ src/app/pages/home/home-page.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { HomeRoutingModule } from './home-routing.module';
-import { PrimaryLayoutModule } from './layouts/primary/primary-layout.module';
-import { SecondaryLayoutModule } from './layouts/secondary/secondary-layout.module';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    PrimaryLayoutModule,
-    SecondaryLayoutModule,
-  ],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomePageModule {}
