8bac260cd85d27bc6c7957e84ef4a64aae2a23eb
Mezame <mezamesoft@gmail.com>
2022-12-02 14:09:51 -0600

    Change modules

--- src/app/pages/home/home-page.module.ts
+++ src/app/pages/home/home-page.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { HomeRoutingModule } from './home-routing.module';
-import { DocumentListTemplateModule } from './templates/document-list/document-list-template.module';
+import { PrimaryLayoutModule } from './layouts/primary/primary-layout.module';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, HomeRoutingModule, DocumentListTemplateModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PrimaryLayoutModule,
+  ],
 })
 export class HomePageModule {}
