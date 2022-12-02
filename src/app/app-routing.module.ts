c27f790064b1ec6e7d6f6a756486bad402f4d8b7
Mezame <mezamesoft@gmail.com>
2022-12-02 14:47:11 -0600

    Change folder name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SelectivePreloadingStrategyService } from './shared/selective-preloading-strategy.service';
+import { SelectivePreloadingStrategyService } from './core/selective-preloading-strategy.service';
 
 const routes: Routes = [
   {
