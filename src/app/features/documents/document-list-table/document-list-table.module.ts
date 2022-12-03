b0ead0d816ffd060528de5f070cf3a8e03063501
Mezame <mezamesoft@gmail.com>
2022-12-02 20:18:52 -0600

    Add document-list-table module

---
+++ src/app/features/documents/document-list-table/document-list-table.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DocumentListTableComponent } from './document-list-table.component';
+
+@NgModule({
+  declarations: [DocumentListTableComponent],
+  imports: [CommonModule],
+  exports: [DocumentListTableComponent],
+})
+export class DocumentListTableModule {}
