4815191ea78f323edc186db14ca1527c742cd63e
Mezame <mezamesoft@gmail.com>
2022-12-03 12:45:11 -0600

    Fix filenames urls

--- src/app/home/layouts/secondary/secondary-layout.component.ts
+++ src/app/home/layouts/secondary/secondary-layout.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-layout',
-  templateUrl: './secondary.layout.html',
-  styleUrls: ['./secondary.layout.sass']
+  templateUrl: './secondary-layout.component.html',
+  styleUrls: ['./secondary-layout.component.sass']
 })
 export class SecondaryLayoutComponent {
 
