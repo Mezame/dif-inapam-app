ffc09732410e731ac4552fa00a32993061289326
Mezame <mezamesoft@gmail.com>
2022-12-03 03:31:04 -0600

    Add document-list-table essential functionality

--- src/app/features/documents/document-list-table/document-list-table.component.ts
+++ src/app/features/documents/document-list-table/document-list-table.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Document } from '../document.interface';
+import { documentsMock } from '../mocks/document.mock';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-document-list-table',
   templateUrl: './document-list-table.component.html',
-  styleUrls: ['./document-list-table.component.sass']
+  styleUrls: ['./document-list-table.component.sass'],
 })
-export class DocumentListTableComponent {
+export class DocumentListTableComponent implements AfterViewInit {
+  documents$: Observable<Document[]> = of(documentsMock);
+  displayedColumns: string[] = [
+    'cardCode',
+    'name',
+    'createDate',
+    'operationCode',
+    'options',
+  ];
 
+  @ViewChild(MatTable)
+  table!: MatTable<Document>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  ngAfterViewInit(): void {
+    const dataSource = new MatTableDataSource<Document>(
+      this.table.dataSource as Document[]
+    );
+    dataSource.paginator = this.paginator;
+    this.table.dataSource = dataSource;
+  }
 }
--- src/app/features/documents/document-list-table/document-list-table.module.ts
+++ src/app/features/documents/document-list-table/document-list-table.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentListTableComponent } from './document-list-table.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [DocumentListTableComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [DocumentListTableComponent],
 })
 export class DocumentListTableModule {}
