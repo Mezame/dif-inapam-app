7f1d16a7c083fd0fa43b05e1cd08b8a1bbaa3071
Mezame <mezamesoft@gmail.com>
2022-12-03 04:55:27 -0600

    Remove unnecessary imports

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { PrimaryLayoutModule } from './layouts/primary/primary-layout.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    PrimaryLayoutModule,
-  ],
+  imports: [HomeRoutingModule, PrimaryLayoutModule],
 })
 export class HomeModule {}
