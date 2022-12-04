a561945c433c0ac4fb97aadf58fd7ea71e49d32a
Mezame <mezamesoft@gmail.com>
2022-12-03 20:55:24 -0600

    Add type MonthNumber

--- src/app/features/documents/pipes/filter-documents-by-month/filter-documents-by-month.pipe.ts
+++ src/app/features/documents/pipes/filter-documents-by-month/filter-documents-by-month.pipe.ts
@@ -2,6 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Document } from '@features/documents/document.interface';
 
+type MonthNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
 @Pipe({
   name: 'filterDocumentsByMonth',
 })
@@ -10,7 +12,7 @@ export class FilterDocumentsByMonthPipe implements PipeTransform {
 
   transform(
     value: Observable<Document[]>,
-    monthNumber: number
+    monthNumber: MonthNumber
   ): Observable<Document[]> {
     this.filteredDocuments$ = value.pipe(
       map((documents) =>
