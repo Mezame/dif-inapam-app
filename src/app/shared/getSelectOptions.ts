3e04b1a076a862eb9b54ad241756c50d2e221c5b
Mezame <mezamesoft@gmail.com>
2022-12-04 16:31:38 -0600

    Add getSelectOptions utils

---
+++ src/app/shared/getSelectOptions.ts
@@ -0,0 +1,9 @@
+export function getSelectOptions(values: string[], viewValues?: string[]) {
+  const selectOptions: { value: string; viewValue: string }[] = [];
+
+  values.forEach((value, i) => {
+    selectOptions.push({ value: value, viewValue: viewValues![i] });
+  });
+
+  return selectOptions;
+}
