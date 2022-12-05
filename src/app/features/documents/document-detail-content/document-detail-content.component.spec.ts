2750b39f13e626fa1a434493ada6c58b97115384
Mezame <mezamesoft@gmail.com>
2022-12-05 17:15:01 -0600

    Add document-detail-content component

---
+++ src/app/features/documents/document-detail-content/document-detail-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocumentDetailContentComponent } from './document-detail-content.component';
+
+describe('DocumentDetailContentComponent', () => {
+  let component: DocumentDetailContentComponent;
+  let fixture: ComponentFixture<DocumentDetailContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DocumentDetailContentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DocumentDetailContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/documents/document-detail-content/document-detail-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-detail-content',
+  templateUrl: './document-detail-content.component.html',
+  styleUrls: ['./document-detail-content.component.scss']
+})
+export class DocumentDetailContentComponent {
+
+}
