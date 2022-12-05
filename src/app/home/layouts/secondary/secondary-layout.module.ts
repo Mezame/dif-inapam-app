4507a87ac21a987fee86d74da3f0b7b705f867dc
Mezame <mezamesoft@gmail.com>
2022-12-04 23:04:10 -0600

    Add secondary-layout essential functionality

--- src/app/home/layouts/secondary/secondary-layout.module.ts
+++ src/app/home/layouts/secondary/secondary-layout.module.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { SecondaryLayoutComponent } from './secondary-layout.component';
 
 @NgModule({
   declarations: [SecondaryLayoutComponent],
-  imports: [],
-  exports: [SecondaryLayoutComponent]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [SecondaryLayoutComponent],
 })
 export class SecondaryLayoutModule {}
