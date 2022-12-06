d5d505143c3ce197a7b745325fda6fc2163f77a3
Mezame <mezamesoft@gmail.com>
2022-12-06 16:46:10 -0600

    Add onpush change detection strategy

--- src/app/features/documents/document-detail-content/document-detail-content.component.ts
+++ src/app/features/documents/document-detail-content/document-detail-content.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Document } from '../document.interface';
 
 @Component({
   selector: 'app-document-detail-content',
   templateUrl: './document-detail-content.component.html',
-  styleUrls: ['./document-detail-content.component.scss']
+  styleUrls: ['./document-detail-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentDetailContentComponent {
-
   @Input('data') document$!: Observable<Document>;
-
 }
