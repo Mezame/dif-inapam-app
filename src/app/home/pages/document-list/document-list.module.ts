6d67617fb178a76af1d533ece301444adaabcbd7
Mezame <mezamesoft@gmail.com>
2022-12-06 12:59:52 -0600

    Add create document button

--- src/app/home/pages/document-list/document-list.module.ts
+++ src/app/home/pages/document-list/document-list.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DocumentListComponent } from './document-list.component';
 import { PrimaryLayoutModule } from '../../layouts/primary/primary-layout.module';
 import { DocumentListTableModule } from '@features/documents/document-list-table/document-list-table.module';
 import { FilterDocumentsByMonthModule } from '@features/documents/pipes/filter-documents-by-month/filter-documents-by-month.module';
 import { SortDocumentsByDateModule } from '@features/documents/pipes/sort-documents-by-date/sort-documents-by-date.module';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [{ path: '', component: DocumentListComponent }];
 
@@ -22,6 +26,8 @@ const routes: Routes = [{ path: '', component: DocumentListComponent }];
     SortDocumentsByDateModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [DocumentListComponent],
 })
