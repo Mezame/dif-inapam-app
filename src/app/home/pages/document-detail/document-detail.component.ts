0f320ade381ae28989c737e7fe0825ec8c3b5044
Mezame <mezamesoft@gmail.com>
2022-12-06 01:41:29 -0600

    Add document-detail essential funcionality

--- src/app/home/pages/document-detail/document-detail.component.ts
+++ src/app/home/pages/document-detail/document-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { getDocumentByCardCodeServiceMock } from '@features/documents/mocks/document.mock';
+import { Document } from '@features/documents/document.interface';
+import { map, Observable, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.component.html',
-  styleUrls: ['./document-detail.component.scss']
+  styleUrls: ['./document-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentDetailComponent {
+  cardCode$!: Observable<string>;
+  document$!: Observable<Document>;
 
+  constructor(private route: ActivatedRoute) {
+    this.cardCode$ = route.params.pipe(map((params) => params['cardCode']));
+
+    this.cardCode$.subscribe(
+      (c) => (this.document$ = of(getDocumentByCardCodeServiceMock(c)))
+    );
+  }
+
+  getDocumentAction(event: { action: string; data: string }) {
+    if (event.action == 'deleteDocument') console.log('Delete document', event.data);
+  }
 }
--- src/app/home/pages/document-detail/document-detail.module.ts
+++ src/app/home/pages/document-detail/document-detail.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentDetailComponent } from './document-detail.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondaryLayoutModule } from '../../layouts/secondary/secondary-layout.module';
+import { DocumentDetailContentModule } from '@features/documents/document-detail-content/document-detail-content.module';
+
 
 const routes: Routes = [{ path: '', component: DocumentDetailComponent }];
 
 @NgModule({
   declarations: [DocumentDetailComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SecondaryLayoutModule,
+    DocumentDetailContentModule
+  ],
 })
 export class DocumentDetailModule {}
