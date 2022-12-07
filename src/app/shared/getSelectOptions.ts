a2162664df35771b555c9907b8c7faf97612392d
Mezame <mezamesoft@gmail.com>
2022-12-06 19:18:50 -0600

    Refactor getSelectOptions

--- src/app/shared/getSelectOptions.ts
+++ src/app/shared/getSelectOptions.ts
@@ -1,8 +1,8 @@
-export function getSelectOptions(values: string[], viewValues?: string[]) {
+export function getSelectOptions(values: string[], viewValues: string[]) {
   const selectOptions: { value: string; viewValue: string }[] = [];
 
   values.forEach((value, i) => {
-    selectOptions.push({ value: value, viewValue: viewValues![i] });
+    selectOptions.push({ value: value, viewValue: viewValues[i] });
   });
 
   return selectOptions;
