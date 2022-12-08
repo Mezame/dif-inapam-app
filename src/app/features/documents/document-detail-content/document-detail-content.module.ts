a340fa7c64f9f8ce9e5182e07ecc3085e7d4acfd
Mezame <mezamesoft@gmail.com>
2022-12-08 10:01:15 -0600

    Change local-date pipe class name

--- src/app/features/documents/document-detail-content/document-detail-content.module.ts
+++ src/app/features/documents/document-detail-content/document-detail-content.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentDetailContentComponent } from './document-detail-content.component';
-import { LocalDateModule } from '@shared/pipes/local-date.module';
+import { LocalDatePipeModule } from '@shared/pipes/local-date/local-date.module';
 
 @NgModule({
   declarations: [DocumentDetailContentComponent],
-  imports: [CommonModule, LocalDateModule],
+  imports: [CommonModule, LocalDatePipeModule],
   exports: [DocumentDetailContentComponent],
 })
 export class DocumentDetailContentModule {}
