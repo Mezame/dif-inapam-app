6c51bcfd1606207cc0a1b0044850cc86cd9dbce5
Mezame <mezamesoft@gmail.com>
2022-12-08 14:59:26 -0600

    Add admin routes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,65 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'reportes',
+        loadChildren: () =>
+          import(
+            './pages/latest-report-detail/latest-report-detail.module'
+          ).then((m) => m.LatestReportDetailModule),
+      },
+      {
+        path: 'reportes/reportes-anteriores',
+        loadChildren: () =>
+          import(
+            './pages/previous-report-list/previous-report-list.module'
+          ).then((m) => m.PreviousReportListModule),
+      },
+      {
+        path: 'oficios/:cardCode/editar',
+        loadChildren: () =>
+          import(
+            '@shared/pages/document-create-edit/document-create-edit.module'
+          ).then((m) => m.DocumentCreateEditModule),
+      },
+      {
+        path: 'asistentes',
+        loadChildren: () =>
+          import('./pages/assistant-list/assistant-list.module').then(
+            (m) => m.AssistantListModule
+          ),
+      },
+      {
+        path: 'asistentes/crear-asistente',
+        loadChildren: () =>
+          import(
+            './pages/assistant-create-edit/assistant-create-edit.module'
+          ).then((m) => m.AssistantCreateEditModule),
+      },
+      {
+        path: 'asistentes/:id/editar',
+        loadChildren: () =>
+          import(
+            './pages/assistant-create-edit/assistant-create-edit.module'
+          ).then((m) => m.AssistantCreateEditModule),
+      },
+      {
+        path: '',
+        redirectTo: 'reportes',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes),
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class AdminRoutingModule {}
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
@@ -27,48 +27,6 @@ const routes: Routes = [
           import(
             './pages/document-detail/document-detail.module'
           ).then((m) => m.DocumentDetailModule),
-      },
-      {
-        path: 'oficios/:cardCode/editar',
-        loadChildren: () =>
-          import(
-            '@shared/pages/document-create-edit/document-create-edit.module'
-          ).then((m) => m.DocumentCreateEditModule),
-      },
-      {
-        path: 'reportes',
-        loadChildren: () =>
-          import(
-            './pages/latest-report-detail/latest-report-detail.module'
-          ).then((m) => m.LatestReportDetailModule),
-      },
-      {
-        path: 'reportes-anteriores',
-        loadChildren: () =>
-          import(
-            './pages/previous-report-list/previous-report-list.module'
-          ).then((m) => m.PreviousReportListModule),
-      },
-      {
-        path: 'asistentes',
-        loadChildren: () =>
-          import(
-            './pages/assistant-list/assistant-list.module'
-          ).then((m) => m.AssistantListModule),
-      },
-      {
-        path: 'crear-asistente',
-        loadChildren: () =>
-          import(
-            './pages/assistant-create-edit/assistant-create-edit.module'
-          ).then((m) => m.AssistantCreateEditModule),
-      },
-      {
-        path: 'asistente/id/editar',
-        loadChildren: () =>
-          import(
-            './pages/assistant-create-edit/assistant-create-edit.module'
-          ).then((m) => m.AssistantCreateEditModule),
       },
       {
         path: '',
