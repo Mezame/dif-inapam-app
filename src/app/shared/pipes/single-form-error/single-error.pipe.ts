272028ec3f242b6549b96434ca5021e33a1ed1e3
Mezame <mezamesoft@gmail.com>
2022-12-08 04:03:55 -0600

    Add interface

--- src/app/shared/pipes/single-form-error/single-error.pipe.ts
+++ src/app/shared/pipes/single-form-error/single-error.pipe.ts
@@ -1,17 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+interface documentFormControlErrors {
+  minlength?: {};
+  maxlength?: {};
+  pattern?: {};
+}
+
 @Pipe({
   name: 'singleError',
 })
 export class SingleErrorPipe implements PipeTransform {
-  transform(errors: any, priorities: string[]) {
+  transform(
+    errors: documentFormControlErrors | null,
+    priorities: string[]
+  ): documentFormControlErrors | null {
     if (!errors) return null;
 
-    const singleError: any = {};
+    const singleError: documentFormControlErrors = {};
 
     priorities.some((priority) => {
-      if (errors[priority]) {
-        singleError[priority] = errors[priority];
+      if (errors[priority as keyof documentFormControlErrors]) {
+        singleError[priority as keyof documentFormControlErrors] =
+          errors[priority as keyof documentFormControlErrors];
 
         return true;
       }
