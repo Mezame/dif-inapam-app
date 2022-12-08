5cbcb44074a5320c79278646b83525b7d5edb271
Mezame <mezamesoft@gmail.com>
2022-12-08 12:17:06 -0600

    Add default-form-value interface

---
+++ src/app/features/documents/document-create-edit-form/document-form-value.interface.ts
@@ -0,0 +1,17 @@
+export interface DocumentFormValue {
+  createDate: Date | null;
+  cardCode: string | null;
+  operationCode: string | null;
+  branchOffice: string | null;
+  reviewDocument: string | null;
+  makeCard: string | null;
+  fathersLastname: string | null;
+  mothersLastname: string | null;
+  name: string | null;
+  sex: string | null;
+  birthdate: Date | null;
+  birthplace: string | null;
+  curp: string | null;
+  maritalStatus: string | null;
+  imageBlob: File | null;
+}
