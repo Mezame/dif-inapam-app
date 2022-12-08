8e07f752f296e5145d869089435708e1ae711298
Mezame <mezamesoft@gmail.com>
2022-12-08 15:24:57 -0600

    Fix secondary-layout module path

--- src/app/home/pages/document-detail/document-detail.module.ts
+++ src/app/home/pages/document-detail/document-detail.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentDetailComponent } from './document-detail.component';
 import { RouterModule, Routes } from '@angular/router';
-import { SecondaryLayoutModule } from '../../layouts/secondary/secondary-layout.module';
+import { SecondaryLayoutModule } from '@shared/layouts/secondary/secondary-layout.module';
 import { DocumentDetailContentModule } from '@features/documents/document-detail-content/document-detail-content.module';
 
 
