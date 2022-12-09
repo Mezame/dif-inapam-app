521d70899cf4088968752a0dca0fece18292eace
Mezame <mezamesoft@gmail.com>
2022-12-09 00:20:43 -0600

    Add document-edit page essential functionality

--- src/app/admin/pages/document-edit/document-edit.component.ts
+++ src/app/admin/pages/document-edit/document-edit.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { getDocumentByCardCodeServiceMock } from '@features/documents/mocks/document.mock';
+import { Document } from '@features/documents/document.interface';
+import { map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.scss'],
 })
-export class DocumentEditComponent {}
+export class DocumentEditComponent implements OnInit {
+  document$!: Observable<Document>;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    const cardCode: string = this.route.snapshot.params['cardCode'];
+
+    this.document$ = of(getDocumentByCardCodeServiceMock(cardCode));
+  }
+
+  getDocumentAction(event: { action: string; data: {} }) {
+    if (event.action == 'editDocument')
+      console.log('Edit document', event.data);
+  }
+}
--- src/app/admin/pages/document-edit/document-edit.module.ts
+++ src/app/admin/pages/document-edit/document-edit.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DocumentEditComponent } from './document-edit.component';
+import { SecondaryLayoutModule } from '@shared/layouts/secondary/secondary-layout.module';
+import { DocumentAddEditFormModule } from '@features/documents/document-add-edit-form/document-add-edit-form.module';
 
 const routes: Routes = [{ path: '', component: DocumentEditComponent }];
 
 @NgModule({
   declarations: [DocumentEditComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SecondaryLayoutModule,
+    DocumentAddEditFormModule,
+  ],
 })
 export class DocumentEditModule {}
