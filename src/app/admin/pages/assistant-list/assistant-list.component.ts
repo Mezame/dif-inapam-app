1cf95813f1acd9018a3d4dabce918d5f3a583c36
Mezame <mezamesoft@gmail.com>
2022-12-09 19:43:47 -0600

    Add essential functionality

--- src/app/admin/pages/assistant-list/assistant-list.component.ts
+++ src/app/admin/pages/assistant-list/assistant-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Assistant } from '@features/assistants/assistant.interface';
+import { assistantsMock } from '@shared/mocks/assistant.mock';
 
 @Component({
   selector: 'app-assistant-list',
   templateUrl: './assistant-list.component.html',
-  styleUrls: ['./assistant-list.component.scss']
+  styleUrls: ['./assistant-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssistantListComponent {
+  assistants$: Observable<Assistant[]> = of(assistantsMock);
 
+  getAssistantAction(event: { action: string; data: {} }) {
+    if (event.action == 'deleteAssistant')
+      console.log('Delete assistant', event.data);
+  }
 }
--- src/app/admin/pages/assistant-list/assistant-list.module.ts
+++ src/app/admin/pages/assistant-list/assistant-list.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AssistantListComponent } from './assistant-list.component';
+import { PrimaryLayoutModule } from '@shared/layouts/primary/primary-layout.module';
+import { AssistantListTableModule } from '@features/assistants/assistant-list-table/assistant-list-table.module';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [{ path: '', component: AssistantListComponent }];
 
 @NgModule({
   declarations: [AssistantListComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    PrimaryLayoutModule,
+    AssistantListTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class AssistantListModule {}
