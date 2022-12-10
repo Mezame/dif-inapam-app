a95069fc8f5acd33f68fd6bbca75078b16659c6b
Mezame <mezamesoft@gmail.com>
2022-12-09 22:57:01 -0600

    Add essential funcionality

--- src/app/admin/pages/assistant-add/assistant-add.component.ts
+++ src/app/admin/pages/assistant-add/assistant-add.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-assistant-add',
   templateUrl: './assistant-add.component.html',
-  styleUrls: ['./assistant-add.component.scss']
+  styleUrls: ['./assistant-add.component.scss'],
 })
 export class AssistantAddComponent {
-
+  getAssistantAction(event: { action: string; data: {} }) {
+    if (event.action == 'addAssistant')
+      console.log('Add assistant', event.data);
+  }
 }
--- src/app/admin/pages/assistant-add/assistant-add.module.ts
+++ src/app/admin/pages/assistant-add/assistant-add.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AssistantAddComponent } from './assistant-add.component';
+import { SecondaryLayoutModule } from '@shared/layouts/secondary/secondary-layout.module';
+import { AssistantAddFormModule } from '@features/assistants/assistant-add-form/assistant-add-form.module';
 
 const routes: Routes = [{ path: '', component: AssistantAddComponent }];
 
 @NgModule({
   declarations: [AssistantAddComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SecondaryLayoutModule,
+    AssistantAddFormModule,
+  ],
 })
 export class AssistantAddModule {}
