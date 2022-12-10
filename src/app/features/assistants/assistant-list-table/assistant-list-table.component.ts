3527f9e8b2e11a5336c4e3511a4e1e5edb174869
Mezame <mezamesoft@gmail.com>
2022-12-09 19:30:23 -0600

    Add assistan-list-table essential functionality

--- src/app/features/assistants/assistant-list-table/assistant-list-table.component.ts
+++ src/app/features/assistants/assistant-list-table/assistant-list-table.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { Assistant } from '../assistant.interface';
 
 @Component({
   selector: 'app-assistant-list-table',
   templateUrl: './assistant-list-table.component.html',
-  styleUrls: ['./assistant-list-table.component.scss']
+  styleUrls: ['./assistant-list-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssistantListTableComponent {
+  displayedColumns = ['name', 'email', 'options'];
 
+  @Input('data') assistants$!: Observable<Assistant[]>;
+
+  @Output('action') actionEvent = new EventEmitter<{
+    action: string;
+    data: string;
+  }>();
+
+  deleteAssistantAction(id: string, action = 'deleteAssistant') {
+    this.actionEvent.emit({ action, data: id });
+  }
 }
--- src/app/features/assistants/assistant-list-table/assistant-list-table.module.ts
+++ src/app/features/assistants/assistant-list-table/assistant-list-table.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AssistantListTableComponent } from './assistant-list-table.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AssistantListTableComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
   exports: [AssistantListTableComponent],
 })
 export class AssistantListTableModule {}
