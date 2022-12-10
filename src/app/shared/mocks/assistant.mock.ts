41fca182f586f39115a7e2bc00811c975a52c2bb
Mezame <mezamesoft@gmail.com>
2022-12-09 18:52:13 -0600

    Add assistant mock

---
+++ src/app/shared/mocks/assistant.mock.ts
@@ -0,0 +1,18 @@
+import { Assistant } from '@features/assistants/assistant.interface';
+
+export const assistantsMock: Assistant[] = [
+  {
+    name: 'Efrain Gómez Arriaga',
+    email: 'asistente@gmail.com',
+    metadata: {
+      id: 'EGA1'
+    }
+  },
+  {
+    name: 'Camila Pérez Montalvo',
+    email: 'asistente2@gmail.com',
+    metadata: {
+      id: 'CPM2'
+    }
+  },
+];
