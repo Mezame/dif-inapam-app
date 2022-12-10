bfd14d5da2e95e90fe058044ed11b35fef94fa41
Mezame <mezamesoft@gmail.com>
2022-12-10 10:57:08 -0600

    Remove primary-layout module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { PrimaryLayoutModule } from '../shared/layouts/primary/primary-layout.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [HomeRoutingModule, PrimaryLayoutModule],
+  imports: [HomeRoutingModule],
 })
 export class HomeModule {}
