23777da5bd28c5265b9ca542109b543dcbcc14a1
Mezame <mezamesoft@gmail.com>
2022-12-10 10:47:50 -0600

    Revert "Remove primary-layout module"
    This reverts commit e363152bcbcbee8126b454ba5275c2fe27795db0.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { PrimaryLayoutModule } from '../shared/layouts/primary/primary-layout.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [HomeRoutingModule, PrimaryLayoutModule],
 })
 export class HomeModule {}
