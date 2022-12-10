480272ef580fa52eaece5c86cbc1083db7c0b02d
Mezame <mezamesoft@gmail.com>
2022-12-09 22:55:52 -0600

    Add invalidEmail property

--- src/app/shared/utils/default-error-message.ts
+++ src/app/shared/utils/default-error-message.ts
@@ -2,4 +2,5 @@ export const defaultErrorMessage = {
   required: 'Este dato es obligatorio.',
   onlyLetters: 'Solo letras.',
   alphanumeric: 'Solo letras y números.',
+  invalidEmail: 'Correo electrónico inválido.'
 };
