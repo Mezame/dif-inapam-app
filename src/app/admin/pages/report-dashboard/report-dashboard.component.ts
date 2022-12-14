caa8865cf107579e188f31d605c133f2be0b731c
Mezame <mezamesoft@gmail.com>
2022-12-14 13:46:08 -0600

    Add essential functionality

--- src/app/admin/pages/report-dashboard/report-dashboard.component.ts
+++ src/app/admin/pages/report-dashboard/report-dashboard.component.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { getLatestReport } from '@features/reports/get-reports';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+import { MatAnchor } from '@angular/material/button';
 import { Report } from '@features/reports/report.interface';
-import { Observable, of } from 'rxjs';
+import { Document } from '@features/documents/document.interface';
+import { SortDocumentsService } from '@features/documents/services/sorts/sort-documents.service';
+import { getLatestReport } from '@features/reports/get-reports';
+import { documentsMock } from '@shared/mocks/document.mock';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { createDownloadUrl } from '../../shared/create-download-url';
 
 @Component({
   selector: 'app-report-dashboard',
@@ -9,6 +20,197 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./report-dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReportDashboardComponent {
+export class ReportDashboardComponent implements OnInit {
   report$: Observable<Report> = of(getLatestReport());
+  documents$?: Observable<Document[]>;
+  objectUrl: string | null = null;
+
+  constructor(
+    private sortDocumentsService: SortDocumentsService,
+    private renderer: Renderer2,
+    private ref: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    const sortedDocuments$ =
+      this.sortDocumentsService.sortDocumentsByCreateDate(
+        of(documentsMock),
+        'des'
+      );
+
+    this.documents$ = this.report$.pipe(
+      switchMap((report) => {
+        const reportYear = new Date(report.date).getFullYear();
+        const reportMonth = new Date(report.date).getMonth();
+
+        return sortedDocuments$.pipe(
+          map((documents) => {
+            return documents.filter((document) => {
+              const documentYear = new Date(document.createDate).getFullYear();
+              const documentMonth = new Date(document.createDate).getMonth();
+
+              return documentYear == reportYear && documentMonth == reportMonth;
+            });
+          })
+        );
+      })
+    );
+  }
+
+  onClick(el: MatAnchor, data: { report: Report; documents?: Document[] }) {
+    createDownloadUrl(el, data, this.objectUrl, this.renderer, this.ref);
+  }
+  /*
+  createDownloadUrl(
+    el: MatAnchor,
+    data: { report: Report; documents?: Document[] }
+  ) {
+    let dataSource: DataSourceCsv;
+    let parsedData: string;
+    let blob: Blob;
+    let fileName: string;
+    const anchor = el._elementRef.nativeElement as HTMLAnchorElement;
+    const reportLocaleMonth = new Date(data.report.date).toLocaleDateString(
+      'es-MX',
+      { month: 'long' }
+    );
+    const reportYear = new Date(data.report.date).getFullYear().toString();
+
+    if (anchor.href) return;
+
+    if (anchor.text.includes('Reporte mensual')) {
+      fileName = `reporte-${reportLocaleMonth}-${reportYear}`;
+
+      dataSource = {
+        header: [
+          'FOLIO DE CREDENCIALES',
+          '',
+          'HOMBRES',
+          'MUJERES',
+          'NUEVO REGISTRO',
+          'CANJE',
+          'REPOSICION',
+          'CANCELADAS',
+          'TOTAL',
+        ],
+        body: [
+          [
+            data.report.cardCodesRange[0],
+            data.report.cardCodesRange[1],
+            data.report.sexStats.male.toString(),
+            data.report.sexStats.female.toString(),
+            data.report.cardsStats.newRecord.toString(),
+            data.report.cardsStats.change.toString(),
+            data.report.cardsStats.replacement.toString(),
+            data.report.cardsStats.cancel.toString(),
+            data.report.cardsStats.getTotal!().toString(),
+          ],
+        ],
+      };
+    }
+
+    if (anchor.text.includes('Reporte de oficios')) {
+      fileName = `reporte-oficios-${reportLocaleMonth}-${reportYear}`;
+
+      dataSource = {
+        header: [
+          'FECHA DE ELABORACION',
+          'FOLIO',
+          'CLAVE MOV',
+          'MODULO',
+          'REVISO DOCUMENTOS',
+          'ELABORO TARJETA',
+          'ENTIDAD FEDERATIVA',
+          'APELLIDO PATERNO',
+          'APELLIDO MATERNO',
+          'NOMBRE',
+          'SEXO H/M',
+          'FECHA DE NACIMIENTO',
+          'ENTIDAD DE NACIMIENTO',
+          'CURP',
+          'ESTADO CIVIL',
+        ],
+        body: [],
+      };
+
+      data.documents!.forEach((document) => {
+        const dateOptions = {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
+        } as Intl.DateTimeFormatOptions;
+        const createDate = new Date(document.createDate).toLocaleDateString(
+          'es-MX',
+          dateOptions
+        );
+        const cardCode = document.cardCode;
+        const operationCode = document.operationCode;
+        const branchOffice = document.branchOffice;
+        const reviewDocument = document.reviewDocument.toUpperCase();
+        const makeCard = document.makeCard.toUpperCase();
+        const fathersLastname = document.fathersLastname.toUpperCase();
+        const mothersLastname = document.mothersLastname.toUpperCase();
+        const name = document.name.toUpperCase();
+        const sex = document.sex.slice(0, 1).toUpperCase();
+        const birthdate = new Date(document.birthdate).toLocaleDateString(
+          'es-MX',
+          dateOptions
+        );
+        const birthplace = document.birthplace.toUpperCase();
+        const curp = document.curp;
+        const maritalStatus = document.maritalStatus.slice(0, 1).toUpperCase();
+        const body = [
+          createDate,
+          cardCode,
+          operationCode,
+          branchOffice,
+          reviewDocument,
+          makeCard,
+          'VERACRUZ',
+          fathersLastname,
+          mothersLastname,
+          name,
+          sex,
+          birthdate,
+          birthplace,
+          curp,
+          maritalStatus,
+        ] as string[];
+
+        dataSource.body.push(body);
+      });
+    }
+
+    if (dataSource!) {
+      parsedData = parseToCsv(dataSource);
+
+      blob = new Blob([parsedData], {
+        type: 'text/csv;charset=utf-8,',
+      });
+
+      this.objectUrl = URL.createObjectURL(blob);
+
+      this.renderer.setAttribute(anchor, 'href', this.objectUrl);
+
+      this.renderer.setAttribute(anchor, 'download', fileName!);
+
+      anchor.click();
+
+      this.renderer.setProperty(el, 'disabled', 'true');
+
+      setTimeout(() => {
+        URL.revokeObjectURL(this.objectUrl as string);
+
+        this.objectUrl = null;
+
+        this.renderer.removeAttribute(anchor, 'href');
+
+        this.renderer.removeAttribute(anchor, 'download');
+
+        this.renderer.setProperty(el, 'disabled', 'false');
+
+        this.ref.markForCheck();
+      }, 2500);
+    }
+  }*/
 }
--- src/app/admin/pages/report-dashboard/report-dashboard.module.ts
+++ src/app/admin/pages/report-dashboard/report-dashboard.module.ts
@@ -5,6 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReportDashboardComponent } from './report-dashboard.component';
 import { PrimaryLayoutModule } from '@shared/layouts/primary/primary-layout.module';
 import { ReportDetailContentModule } from '@features/reports/report-detail-content/report-detail-content.module';
+import { DocumentListTableModule } from '@features/documents/document-list-table/document-list-table.module';
+import { SortDocumentsService } from '@features/documents/services/sorts/sort-documents.service';
+import { SortDocumentsByDatePipeModule } from '@features/documents/pipes/sort-documents-by-date/sort-documents-by-date.module';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,8 +21,11 @@ const routes: Routes = [{ path: '', component: ReportDashboardComponent }];
     RouterModule.forChild(routes),
     PrimaryLayoutModule,
     ReportDetailContentModule,
+    DocumentListTableModule,
+    SortDocumentsByDatePipeModule,
     MatButtonModule,
     MatIconModule,
   ],
+  providers: [SortDocumentsService],
 })
 export class ReportDashboardModule {}
