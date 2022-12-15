d983f1d246ae137b1e3e9d46e66a49ac2d2eb4a5
Mezame <mezamesoft@gmail.com>
2022-12-15 14:07:31 -0600

    Refactor code

--- src/app/admin/shared/create-download-url.ts
+++ src/app/admin/shared/create-download-url.ts
@@ -16,115 +16,118 @@ export function createDownloadUrl(
   let blob: Blob;
   let fileName: string;
   const anchor = el._elementRef.nativeElement as HTMLAnchorElement;
-  const reportLocaleMonth = new Date(data.report.date).toLocaleDateString(
-    'es-MX',
-    { month: 'long' }
-  );
-  const reportYear = new Date(data.report.date).getFullYear().toString();
 
   if (anchor.href) return;
 
-  if (!data.documents) {
-    fileName = `reporte-${reportLocaleMonth}-${reportYear}`;
+  if (data) {
+    const reportLocaleMonth = new Date(data.report.date).toLocaleDateString(
+      'es-MX',
+      { month: 'long' }
+    );
+    const reportYear = new Date(data.report.date).getFullYear().toString();
 
-    dataSource = {
-      header: [
-        'FOLIO DE CREDENCIALES',
-        '',
-        'HOMBRES',
-        'MUJERES',
-        'NUEVO REGISTRO',
-        'CANJE',
-        'REPOSICION',
-        'CANCELADAS',
-        'TOTAL',
-      ],
-      body: [
-        [
-          data.report.cardCodesRange[0],
-          data.report.cardCodesRange[1],
-          data.report.sexStats.male.toString(),
-          data.report.sexStats.female.toString(),
-          data.report.cardsStats.newRecord.toString(),
-          data.report.cardsStats.change.toString(),
-          data.report.cardsStats.replacement.toString(),
-          data.report.cardsStats.cancel.toString(),
-          data.report.cardsStats.getTotal!().toString(),
+    if (!data.documents) {
+      fileName = `reporte-${reportLocaleMonth}-${reportYear}`;
+
+      dataSource = {
+        header: [
+          'FOLIO DE CREDENCIALES',
+          '',
+          'HOMBRES',
+          'MUJERES',
+          'NUEVO REGISTRO',
+          'CANJE',
+          'REPOSICION',
+          'CANCELADAS',
+          'TOTAL',
         ],
-      ],
-    };
-  }
+        body: [
+          [
+            data.report.cardCodesRange[0],
+            data.report.cardCodesRange[1],
+            data.report.sexStats.male.toString(),
+            data.report.sexStats.female.toString(),
+            data.report.cardsStats.newRecord.toString(),
+            data.report.cardsStats.change.toString(),
+            data.report.cardsStats.replacement.toString(),
+            data.report.cardsStats.cancel.toString(),
+            data.report.cardsStats.getTotal!().toString(),
+          ],
+        ],
+      };
+    }
 
-  if (data.documents) {
-    fileName = `reporte-oficios-${reportLocaleMonth}-${reportYear}`;
+    if (data.documents) {
+      fileName = `reporte-oficios-${reportLocaleMonth}-${reportYear}`;
 
-    dataSource = {
-      header: [
-        'FECHA DE ELABORACION',
-        'FOLIO',
-        'CLAVE MOV',
-        'MODULO',
-        'REVISO DOCUMENTOS',
-        'ELABORO TARJETA',
-        'ENTIDAD FEDERATIVA',
-        'APELLIDO PATERNO',
-        'APELLIDO MATERNO',
-        'NOMBRE',
-        'SEXO H/M',
-        'FECHA DE NACIMIENTO',
-        'ENTIDAD DE NACIMIENTO',
-        'CURP',
-        'ESTADO CIVIL',
-      ],
-      body: [],
-    };
+      dataSource = {
+        header: [
+          'FECHA DE ELABORACION',
+          'FOLIO',
+          'CLAVE MOV',
+          'MODULO',
+          'REVISO DOCUMENTOS',
+          'ELABORO TARJETA',
+          'ENTIDAD FEDERATIVA',
+          'APELLIDO PATERNO',
+          'APELLIDO MATERNO',
+          'NOMBRE',
+          'SEXO H/M',
+          'FECHA DE NACIMIENTO',
+          'ENTIDAD DE NACIMIENTO',
+          'CURP',
+          'ESTADO CIVIL',
+        ],
+        body: [],
+      };
 
-    data.documents!.forEach((document) => {
-      const dateOptions = {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-      } as Intl.DateTimeFormatOptions;
-      const createDate = new Date(document.createDate).toLocaleDateString(
-        'es-MX',
-        dateOptions
-      );
-      const cardCode = document.cardCode;
-      const operationCode = document.operationCode;
-      const branchOffice = document.branchOffice;
-      const reviewDocument = document.reviewDocument.toUpperCase();
-      const makeCard = document.makeCard.toUpperCase();
-      const fathersLastname = document.fathersLastname.toUpperCase();
-      const mothersLastname = document.mothersLastname.toUpperCase();
-      const name = document.name.toUpperCase();
-      const sex = document.sex.slice(0, 1).toUpperCase();
-      const birthdate = new Date(document.birthdate).toLocaleDateString(
-        'es-MX',
-        dateOptions
-      );
-      const birthplace = document.birthplace.toUpperCase();
-      const curp = document.curp;
-      const maritalStatus = document.maritalStatus.slice(0, 1).toUpperCase();
-      const body = [
-        createDate,
-        cardCode,
-        operationCode,
-        branchOffice,
-        reviewDocument,
-        makeCard,
-        'VERACRUZ',
-        fathersLastname,
-        mothersLastname,
-        name,
-        sex,
-        birthdate,
-        birthplace,
-        curp,
-        maritalStatus,
-      ] as string[];
+      data.documents!.forEach((document) => {
+        const dateOptions = {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
+        } as Intl.DateTimeFormatOptions;
+        const createDate = new Date(document.createDate).toLocaleDateString(
+          'es-MX',
+          dateOptions
+        );
+        const cardCode = document.cardCode;
+        const operationCode = document.operationCode;
+        const branchOffice = document.branchOffice;
+        const reviewDocument = document.reviewDocument.toUpperCase();
+        const makeCard = document.makeCard.toUpperCase();
+        const fathersLastname = document.fathersLastname.toUpperCase();
+        const mothersLastname = document.mothersLastname.toUpperCase();
+        const name = document.name.toUpperCase();
+        const sex = document.sex.slice(0, 1).toUpperCase();
+        const birthdate = new Date(document.birthdate).toLocaleDateString(
+          'es-MX',
+          dateOptions
+        );
+        const birthplace = document.birthplace.toUpperCase();
+        const curp = document.curp;
+        const maritalStatus = document.maritalStatus.slice(0, 1).toUpperCase();
+        const body = [
+          createDate,
+          cardCode,
+          operationCode,
+          branchOffice,
+          reviewDocument,
+          makeCard,
+          'VERACRUZ',
+          fathersLastname,
+          mothersLastname,
+          name,
+          sex,
+          birthdate,
+          birthplace,
+          curp,
+          maritalStatus,
+        ] as string[];
 
-      dataSource.body.push(body);
-    });
+        dataSource.body.push(body);
+      });
+    }
   }
 
   if (dataSource!) {
