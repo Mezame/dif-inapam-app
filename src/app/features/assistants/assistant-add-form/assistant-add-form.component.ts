97abab1a6ea4ead0addeed510c6dc5d00651f7c3
Mezame <mezamesoft@gmail.com>
2022-12-15 17:26:49 -0600

    Refactor code

--- src/app/features/assistants/assistant-add-form/assistant-add-form.component.ts
+++ src/app/features/assistants/assistant-add-form/assistant-add-form.component.ts
@@ -2,9 +2,10 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  OnInit,
   Output,
 } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { defaultErrorMessage } from '@shared/utils/default-error-message';
 
 interface AssistantFormValue {
@@ -18,16 +19,10 @@ interface AssistantFormValue {
   styleUrls: ['./assistant-add-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AssistantAddFormComponent {
-  defaultErrorMessage = defaultErrorMessage;
+export class AssistantAddFormComponent implements OnInit {
+  defaultErrorMessage: any;
 
-  assistantForm = this.fb.group({
-    name: [
-      '',
-      [Validators.required, Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*')],
-    ],
-    email: ['', [Validators.required, Validators.email]],
-  });
+  assistantForm!: FormGroup<any>;
 
   @Output() actionEvent = new EventEmitter<{
     action: string;
@@ -42,6 +37,18 @@ export class AssistantAddFormComponent {
 
   get emailCtrl() {
     return this.assistantForm.controls['email'];
+  }
+
+  ngOnInit(): void {
+    this.defaultErrorMessage = defaultErrorMessage;
+
+    this.assistantForm = this.fb.group({
+      name: [
+        '',
+        [Validators.required, Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*')],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
   onSubmit() {
