303d904670a5bac5d87814e8e59aadb609db00ee
Mezame <mezamesoft@gmail.com>
2022-12-15 00:18:19 -0600

    Add sorting output

--- src/app/features/documents/utils/get-create-date.ts
+++ src/app/features/documents/utils/get-create-date.ts
@@ -1,50 +1,63 @@
 import { Observable, tap } from 'rxjs';
 import { Document } from '@features/documents/document.interface';
+import { number } from 'echarts/core';
 
-export function getMonthsNumbers(documents: Observable<Document[]>, year: string): string[] {
-  let months: string[] = [];
+export function getMonthsNumbers(
+  documents: Observable<Document[]>,
+  year: string
+): string[] {
+  let monthsNumbers: string[] = [];
 
   documents
     .pipe(
       tap((documents) => {
         documents.forEach((document) => {
           const createDate = new Date(document.createDate);
-          const monthNumber = (createDate.getMonth() + 1).toString();
+          const documentMonth = (createDate.getMonth() + 1).toString();
           const documentYear = createDate.getFullYear().toString();
 
-          if (!months.includes(monthNumber) && year == documentYear) {
-            months.push(monthNumber);
+          if (!monthsNumbers.includes(documentMonth) && year == documentYear) {
+            monthsNumbers.push(documentMonth);
           }
         });
+
+        monthsNumbers.sort((a, b) => parseInt(a) - parseInt(b));
       })
     )
     .subscribe();
 
-  return months;
+  return monthsNumbers;
 }
 
-export function getMonthsWords(documents: Observable<Document[]>, year: string): string[] {
-  let months: string[] = [];
+export function getMonthsWords(
+  documents: Observable<Document[]>,
+  year: string
+): string[] {
+  let monthsWords: string[] = [];
 
   documents
     .pipe(
       tap((documents) => {
+        documents.sort(
+          (a, b) => Date.parse(a.createDate) - Date.parse(b.createDate)
+        );
+
         documents.forEach((document) => {
           const createDate = new Date(document.createDate);
-          const monthWord = createDate.toLocaleDateString('es-MX', {
+          const documentMonth = createDate.toLocaleDateString('es-MX', {
             month: 'long',
           });
           const documentYear = createDate.getFullYear().toString();
 
-          if (!months.includes(monthWord) && year == documentYear) {
-            months.push(monthWord);
+          if (!monthsWords.includes(documentMonth) && year == documentYear) {
+            monthsWords.push(documentMonth);
           }
         });
       })
     )
     .subscribe();
 
-  return months;
+  return monthsWords;
 }
 
 export function getYears(documents: Observable<Document[]>): string[] {
@@ -61,6 +74,8 @@ export function getYears(documents: Observable<Document[]>): string[] {
             years.push(documentYear);
           }
         });
+
+        years.sort((a, b) => parseInt(a) - parseInt(b));
       })
     )
     .subscribe();
