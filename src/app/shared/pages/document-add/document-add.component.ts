0b95a437b2b62d69080b1e8f7d91992e0e595f63
Mezame <mezamesoft@gmail.com>
2022-12-15 14:31:12 -0600

    Change detection strategy

--- src/app/shared/pages/document-add/document-add.component.ts
+++ src/app/shared/pages/document-add/document-add.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-document-add',
   templateUrl: './document-add.component.html',
   styleUrls: ['./document-add.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentAddComponent {
   getDocumentAction(event: { action: string; data: {} }) {
