2b5716ad34f3be1ff4078990b8ec00e6658432eb
Mezame <mezamesoft@gmail.com>
2022-12-15 13:54:34 -0600

    Refactor code

--- src/app/admin/pages/assistant-list/assistant-list.component.ts
+++ src/app/admin/pages/assistant-list/assistant-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Assistant } from '@features/assistants/assistant.interface';
 import { assistantsMock } from '@mocks/assistant.mock';
@@ -9,8 +9,12 @@ import { assistantsMock } from '@mocks/assistant.mock';
   styleUrls: ['./assistant-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AssistantListComponent {
-  assistants$: Observable<Assistant[]> = of(assistantsMock);
+export class AssistantListComponent implements OnInit {
+  assistants$!: Observable<Assistant[]>;
+
+  ngOnInit(): void {
+    this.assistants$ = of(assistantsMock);
+  }
 
   getAssistantAction(event: { action: string; data: {} }) {
     if (event.action == 'deleteAssistant')
