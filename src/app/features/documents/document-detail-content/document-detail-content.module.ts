c95d8de9d102e5ae7543c0f9fef5a33fb9f60a8e
Mezame <mezamesoft@gmail.com>
2022-12-15 22:58:56 -0600

    Remove localDate pipe

--- src/app/features/documents/document-detail-content/document-detail-content.module.ts
+++ src/app/features/documents/document-detail-content/document-detail-content.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DocumentDetailContentComponent } from './document-detail-content.component';
-import { LocalDatePipeModule } from '@shared/pipes/local-date/local-date.module';
 
 @NgModule({
   declarations: [DocumentDetailContentComponent],
-  imports: [CommonModule, LocalDatePipeModule],
+  imports: [CommonModule],
   exports: [DocumentDetailContentComponent],
 })
 export class DocumentDetailContentModule {}
