1291b86e58d3162af60140cfba1d6340347a23fa
Mezame <mezamesoft@gmail.com>
2022-12-18 14:52:23 -0600

    Add firestore get-documents service

---
+++ src/app/features/documents/services/firestore/get-documents.service.ts
@@ -0,0 +1,105 @@
+import { Injectable } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  query,
+  where,
+  getDocs,
+  CollectionReference,
+  DocumentData,
+  doc,
+  getDoc,
+} from '@angular/fire/firestore';
+import { DateStore } from '@features/documents/date-store.interface';
+import { Document } from '@features/documents/document.interface';
+import { catchError, from, map, Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class GetDocumentsService {
+  private documentsCollectionRef = collection(
+    this.firestore,
+    'documents'
+  ) as CollectionReference<DocumentData>;
+
+  constructor(private firestore: Firestore) {}
+
+  getDocuments(): Observable<Document[]> {
+    const documents$ = collectionData(
+      this.documentsCollectionRef
+    ) as Observable<Document[]>;
+
+    return documents$.pipe(
+      catchError(
+        this.handleError<Document[]>('GetDocumentService', 'getDocuments', [])
+      )
+    );
+  }
+
+  getDocumentByCardCode(cardCode: string): Observable<Document> {
+    const cardCodeQuery = query(
+      this.documentsCollectionRef,
+      where('cardCode', '==', cardCode)
+    );
+
+    const querySnapshot$ = from(getDocs(cardCodeQuery));
+
+    const document$ = querySnapshot$.pipe(
+      map((querySnapshot) => {
+        let document;
+
+        querySnapshot.forEach((doc) => (document = doc.data()));
+
+        return document as unknown as Document;
+      })
+    );
+
+    return document$.pipe(
+      tap((document) => {
+        if (!document) throw new Error('could not get document');
+      }),
+      catchError(
+        this.handleError<Document>(
+          'GetDocumentService',
+          `getDocumentByCardCode w/ cardCode=${cardCode}`
+        )
+      )
+    );
+  }
+
+  getDocumentUtilsDateStore(): Observable<DateStore> {
+    const docRef = doc(this.firestore, 'documentUtils', 'dateStore');
+
+    const docSnap$ = from(getDoc(docRef));
+
+    const dateStore$ = docSnap$.pipe(
+      map((createDate) => {
+        return createDate.data() as DateStore;
+      })
+    );
+
+    return dateStore$.pipe(
+      tap((dateStore) => {
+        if (!dateStore) throw new Error('could not get dateStore');
+      }),
+      catchError(
+        this.handleError<DateStore>(
+          'GetDocumentService',
+          'getDocumentUtilsDateStore'
+        )
+      )
+    );
+  }
+
+  private handleError<T>(
+    serviceName = '',
+    operation = 'operation',
+    result?: T
+  ) {
+    return (error: any): Observable<T> => {
+      console.log(`${serviceName}: ${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
