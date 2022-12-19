eef6e752335a44e6279527967ec381881ce93b0a
Mezame <mezamesoft@gmail.com>
2022-12-19 13:47:17 -0600

    Implement add-document service

--- src/app/shared/pages/document-add/document-add.component.ts
+++ src/app/shared/pages/document-add/document-add.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Document } from '@features/documents/document.interface';
+import { AddDocumentsService } from '@features/documents/services/firestore/add-documents.service';
+import { DocumentStoreService } from '@features/documents/services/firestore/store/document-store.service';
 
 @Component({
   selector: 'app-document-add',
@@ -7,8 +10,48 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentAddComponent {
+  constructor(
+    private addDocumentsService: AddDocumentsService,
+    private documentStoreService: DocumentStoreService
+  ) {}
+
   getDocumentAction(event: { action: string; data: {} }) {
-    if (event.action == 'addDocument')
-      console.log('Add document', event.data);
+    if (event.action == 'addDocument') {
+      const formData = { ...event.data };
+
+      const document = this.cleanFormData(formData);
+
+      const imageBlob = (event.data as any).imageObj.blob as File;
+
+      if (imageBlob) {
+        console.log('Upload image to server', imageBlob);
+      }
+
+      if (document) {
+        console.log('Add document', document);
+
+        this.addDocumentsService.addDocument(document).subscribe((docRef) => {
+          if (docRef.id) {
+            this.documentStoreService.addDocument(document);
+          }
+        });
+      }
+    }
+  }
+
+  cleanFormData(formData: any): Document | undefined {
+    if (!formData) return;
+
+    const document = formData;
+
+    delete document.imageObj;
+
+    for (const data in document) {
+      if (document[data] == null || document[data] == undefined) {
+        delete document[data];
+      }
+    }
+
+    return document as Document;
   }
 }
--- src/app/shared/pages/document-add/document-add.module.ts
+++ src/app/shared/pages/document-add/document-add.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DocumentAddComponent } from './document-add.component';
-import { SecondaryLayoutModule } from '@shared/layouts/secondary/secondary-layout.module';
 import { DocumentAddEditFormModule } from '@features/documents/document-add-edit-form/document-add-edit-form.module';
+import { AddDocumentsService } from '@features/documents/services/firestore/add-documents.service';
+import { GetDocumentsService } from '@features/documents/services/firestore/get-documents.service';
+import { DocumentStoreService } from '@features/documents/services/firestore/store/document-store.service';
+import { SecondaryLayoutModule } from '@shared/layouts/secondary/secondary-layout.module';
+import { DocumentAddComponent } from './document-add.component';
 
 const routes: Routes = [{ path: '', component: DocumentAddComponent }];
 
@@ -16,5 +19,6 @@ const routes: Routes = [{ path: '', component: DocumentAddComponent }];
     SecondaryLayoutModule,
     DocumentAddEditFormModule,
   ],
+  providers: [AddDocumentsService, DocumentStoreService, GetDocumentsService],
 })
 export class DocumentAddModule {}
