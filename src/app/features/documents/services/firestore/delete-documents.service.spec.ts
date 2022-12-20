c3fe2efa82bfd449eefe8f297314209e068d9de1
Mezame <mezamesoft@gmail.com>
2022-12-20 10:42:58 -0600

    Add delete-document service functionality

---
+++ src/app/features/documents/services/firestore/delete-documents.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DeleteDocumentsService } from './delete-documents.service';
+
+describe('DeleteDocumentsService', () => {
+  let service: DeleteDocumentsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DeleteDocumentsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/features/documents/services/firestore/delete-documents.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { deleteDoc, doc, Firestore } from '@angular/fire/firestore';
+import { catchError, from, map, Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class DeleteDocumentsService {
+  constructor(private firestore: Firestore) {}
+
+  deleteDocument(id: string): Observable<unknown> {
+    const docRef = doc(this.firestore, 'documents/' + id);
+
+    const documentRef$ = from(deleteDoc(docRef));
+
+    return documentRef$.pipe(
+      map((docRef) => {
+        if (docRef == undefined) {
+          return id;
+        } else {
+          throw new Error('could not delete document');
+        }
+      }),
+      tap((_) => {
+        console.log(`deleted document w/ id=${id}`);
+      }),
+      catchError(
+        this.handleError<unknown>('DeleteDocumentsService', 'deleteDocument')
+      )
+    );
+  }
+
+  private handleError<T>(
+    serviceName = '',
+    operation = 'operation',
+    result = {} as T
+  ) {
+    return (error: any): Observable<T> => {
+      console.log(`${serviceName}: ${operation} failed: ${error.message}`);
+
+      return of(result);
+    };
+  }
+}
