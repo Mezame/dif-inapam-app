8129237e3a7e0f6cf505ec6373b9e7af3fda005f
Mezame <mezamesoft@gmail.com>
2022-12-21 12:40:13 -0600

    Add get-reports service functionality

---
+++ src/app/features/reports/services/firestore/get-reports.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  collection,
+  collectionData,
+  CollectionReference,
+  DocumentData,
+  Firestore,
+} from '@angular/fire/firestore';
+import { Report } from '@features/reports/report.interface';
+import { catchError, Observable, of } from 'rxjs';
+
+@Injectable()
+export class GetReportsService {
+  private reportsCollectionRef = collection(
+    this.firestore,
+    'reports'
+  ) as CollectionReference<DocumentData>;
+
+  constructor(private firestore: Firestore) {}
+
+  getReports(): Observable<Report[]> {
+    const reports$ = collectionData(this.reportsCollectionRef) as Observable<
+      Report[]
+    >;
+
+    return reports$.pipe(
+      catchError(
+        this.handleError<Report[]>('GetReportsService', 'getReports', [])
+      )
+    );
+  }
+
+  private handleError<T>(
+    serviceName = '',
+    operation = 'operation',
+    result = {} as T
+  ) {
+    return (error: any): Observable<T> => {
+      console.log(`${serviceName}: ${operation} failed: ${error.message}`);
+
+      return of(result);
+    };
+  }
+}
