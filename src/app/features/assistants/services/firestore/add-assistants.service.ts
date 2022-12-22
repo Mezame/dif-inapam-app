19ca9468a17e18f57205ece4a258b0c0fd33505c
Mezame <mezamesoft@gmail.com>
2022-12-22 13:01:59 -0600

    Add set-assistant functionality

--- src/app/features/assistants/services/firestore/add-assistants.service.ts
+++ src/app/features/assistants/services/firestore/add-assistants.service.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@angular/core';
 import {
   addDoc,
   collection,
+  CollectionReference,
+  doc,
   DocumentData,
   DocumentReference,
   Firestore,
+  serverTimestamp,
+  setDoc,
 } from '@angular/fire/firestore';
 import { Assistant } from '@features/assistants/assistant.interface';
-import { catchError, from, Observable, of, take, tap } from 'rxjs';
+import { catchError, from, map, Observable, of, take, tap } from 'rxjs';
 
 @Injectable()
 export class AddAssistantsService {
+  assistantsCollection = collection(
+    this.firestore,
+    'assistants'
+  ) as CollectionReference<DocumentData>;
+
   constructor(private firestore: Firestore) {}
 
   addAssistant(
     assistant: Assistant
   ): Observable<DocumentReference<DocumentData>> {
-    const assistantsCollection = collection(this.firestore, 'assistants');
-
-    const assistantRef$ = from(addDoc(assistantsCollection, assistant));
+    const assistantRef$ = from(addDoc(this.assistantsCollection, assistant));
 
     return assistantRef$.pipe(
       take(1),
@@ -31,6 +38,57 @@ export class AddAssistantsService {
           'addAssistant'
         )
       )
+    );
+  }
+
+  setAssistant(assistant: Assistant, id?: string) {
+    let assistantRef$: Observable<any>;
+    let assistantId: string;
+
+    if (id) {
+      assistantId = id;
+      const docRef = doc(this.firestore, 'assistants', assistantId);
+
+      assistantRef$ = from(setDoc(docRef, assistant));
+
+      assistantRef$.pipe(
+        map((docRef) => {
+          if (docRef == undefined) {
+            return assistantId;
+          } else {
+            throw new Error('could not set assistant');
+          }
+        })
+      );
+    } else {
+      const docRef = doc(this.assistantsCollection);
+      assistantId = docRef.id;
+
+      assistantRef$ = from(
+        setDoc(docRef, {
+          ...assistant,
+          metadata: {
+            id: assistantId,
+            timestamp: serverTimestamp(),
+          },
+        })
+      ).pipe(
+        map((_) => {
+          if (assistantId) {
+            return assistantId;
+          } else {
+            throw new Error('could not set assistant');
+          }
+        })
+      );
+    }
+
+    return assistantRef$.pipe(
+      take(1),
+      tap((_) => {
+        console.log(`setted assistant w/ id=${assistantId}`);
+      }),
+      catchError(this.handleError('AddAssistantsService', 'setAssistant'))
     );
   }
 
