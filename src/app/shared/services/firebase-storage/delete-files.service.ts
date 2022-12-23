99d474ef746f843546c4f83988ebdfe08b10baf7
Mezame <mezamesoft@gmail.com>
2022-12-23 00:09:28 -0600

    Add delete-files service functionality

---
+++ src/app/shared/services/firebase-storage/delete-files.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { deleteObject, ref, Storage } from '@angular/fire/storage';
+import { catchError, from, map, Observable, of, take, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DeleteFilesService {
+  constructor(private fireStorage: Storage) {}
+
+  deleteFile(filename: string): Observable<boolean> {
+    const storageRef = ref(this.fireStorage, `documents/${filename}`);
+
+    const deleteResult$ = from(deleteObject(storageRef)).pipe(
+      map((deleteResult) => {
+        if (deleteResult == undefined) {
+          return true;
+        } else {
+          throw new Error('could not delete document');
+        }
+      })
+    );
+
+    return deleteResult$.pipe(
+      take(1),
+      tap((_) => console.log('deleted file')),
+      catchError(
+        this.handleError<boolean>('DeleteFilesService', 'deleteFile')
+      )
+    );
+  }
+
+  private handleError<T>(
+    serviceName = '',
+    operation = 'operation',
+    result = {} as T
+  ) {
+    return (error: any): Observable<T> => {
+      console.log(`${serviceName}: ${operation} failed: ${error.message}`);
+
+      return of(result);
+    };
+  }
+}
