7caff02cf26c209bcd2f7ec29c315862c875d9ec
Mezame <mezamesoft@gmail.com>
2022-12-22 19:31:36 -0600

    Implement upload-files service

--- src/app/shared/pages/document-add/document-add.component.ts
+++ src/app/shared/pages/document-add/document-add.component.ts
@@ -4,6 +4,7 @@ import { DocumentFormValue } from '@features/documents/document-add-edit-form/do
 import { Document } from '@features/documents/document.interface';
 import { AddDocumentsService } from '@features/documents/services/firestore/add-documents.service';
 import { DocumentStoreService } from '@features/documents/services/store/document-store.service';
+import { UploadFilesService } from '@shared/services/firebase-storage/upload-files.service';
 
 @Component({
   selector: 'app-document-add',
@@ -15,6 +16,7 @@ export class DocumentAddComponent {
   constructor(
     private addDocumentsService: AddDocumentsService,
     private documentStoreService: DocumentStoreService,
+    private uploadFilesService: UploadFilesService,
     private router: Router
   ) {}
 
@@ -27,22 +29,32 @@ export class DocumentAddComponent {
       const imageBlob = formData.imageObj?.blob as File;
 
       if (imageBlob) {
-        console.log('Upload image to server', imageBlob);
-      }
-
-      if (document) {
-        console.log('Add document', document);
-
-        this.addDocumentsService
-          .setDocument(document.cardCode, document)
-          .subscribe((docRef) => {
-            if (docRef == document.cardCode) {
-              this.documentStoreService.addDocument(document);
-
-              this.router.navigate(['/home/oficios']);
+        this.uploadFilesService
+          .uploadFromBlob(imageBlob, document.cardCode)
+          .subscribe((downloadUrl) => {
+            if (typeof downloadUrl == 'string') {
+              document.imageUrl = downloadUrl;
             }
+
+            this.addDocument(document);
           });
+      } else {
+        this.addDocument(document);
       }
+    }
+  }
+
+  addDocument(document: Document) {
+    if (document) {
+      this.addDocumentsService
+        .setDocument(document.cardCode, document)
+        .subscribe((docRef) => {
+          if (docRef == document.cardCode) {
+            this.documentStoreService.addDocument(document);
+
+            this.router.navigate(['/home/oficios']);
+          }
+        });
     }
   }
 
