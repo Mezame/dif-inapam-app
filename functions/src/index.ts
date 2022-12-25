aa9d7b596fcac41569d566db0e4ed5637b20766c
Mezame <mezamesoft@gmail.com>
2022-12-25 13:11:54 -0600

    Add on-write-firestore-trigger function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let onWriteDocuments: any;
+
+export const helloWorld = functions.https.onRequest((_request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
+
+exports.functions = functions.firestore
+  .document('documents/{id}')
+  .onWrite(async (change, context) => {
+
+    //const document = change.after.exists ? change.after.data() : null;
+
+    onWriteDocuments =
+      onWriteDocuments ||
+      await (
+        await import('./firestore-triggers/on-write-documents-set-date-store')
+      ).default(change, context);
+  });
