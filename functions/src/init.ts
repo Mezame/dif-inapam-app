82b0e68b4c1664f3ba959b485734e77b17953258
Mezame <mezamesoft@gmail.com>
2022-12-28 14:38:29 -0600

    Add auth init export

--- functions/src/init.ts
+++ functions/src/init.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
 const app = initializeApp();
 
 export const firestore = getFirestore(app);
+export const auth = getAuth(app);
