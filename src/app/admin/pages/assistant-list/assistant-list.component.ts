c8bbf74e2e7e2fce94751d17b728e3228b5aad4e
Mezame <mezamesoft@gmail.com>
2022-12-28 11:52:40 -0600

    Add delete-assistant store service functionality

--- src/app/admin/pages/assistant-list/assistant-list.component.ts
+++ src/app/admin/pages/assistant-list/assistant-list.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Assistant } from '@features/assistants/assistant.interface';
 import { DeleteAssistantsService } from '@features/assistants/services/firestore/delete-assistants.service';
 import { AssistantStoreService } from '@features/assistants/services/store/assistant-store.service';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-assistant-list',
@@ -29,7 +29,11 @@ export class AssistantListComponent implements OnInit {
       const id = assistant.metadata.id;
 
       if (confirm('¿Confirmas eliminar asistente?') == true) {
-        this.deleteAssistantsService.deleteAssistant(id).subscribe();
+        this.deleteAssistantsService.deleteAssistant(id).subscribe((res) => {
+          if (res == true) {
+            this.assistantStoreService.deleteAssistant(id);
+          }
+        });
       }
     }
   }
--- src/app/features/assistants/services/store/assistant-store.service.ts
+++ src/app/features/assistants/services/store/assistant-store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Assistant } from '@features/assistants/assistant.interface';
-import { BehaviorSubject, Observable, take } from 'rxjs';
+import { BehaviorSubject, map, Observable, take, tap } from 'rxjs';
 import { GetAssistantsService } from '../firestore/get-assistants.service';
 
 @Injectable()
@@ -18,6 +18,7 @@ export class AssistantStoreService {
   }
 
   getAssistants(): Observable<Assistant[]> {
+    const done = false;
     this.assistants$.pipe(take(2)).subscribe((assistants) => {
       if (assistants.length < 1) {
         this.loadAssistants();
@@ -25,5 +26,15 @@ export class AssistantStoreService {
     });
 
     return this.assistants$;
+  }
+
+  deleteAssistant(id: string) {
+    const assistants$ = this.getAssistants();
+
+    assistants$.pipe(take(1)).subscribe((assistants) => {
+      if (assistants.length == 1 && assistants[0].metadata.id == id) {
+        this.assistants$.next([]);
+      }
+    });
   }
 }
