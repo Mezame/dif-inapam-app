add4f68887cbe25abf5ebc7d12e876c226b9c9ea
Mezame <mezamesoft@gmail.com>
2022-12-31 00:13:38 -0600

    Implement update & delete document store services and refactor code

--- src/app/home/pages/document-detail/document-detail.component.ts
+++ src/app/home/pages/document-detail/document-detail.component.ts
@@ -4,7 +4,7 @@ import { Document } from '@features/documents/document.interface';
 import { DeleteDocumentsService } from '@features/documents/services/firestore/delete-documents.service';
 import { DocumentStoreService } from '@features/documents/services/store/document-store.service';
 import { UpdateDocumentsService } from '@features/documents/services/firestore/update-documents.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { DeleteFilesService } from '@shared/services/firebase-storage/delete-files.service';
 import { FireAuthService } from '@core/auth/fire-auth.service';
 
@@ -15,8 +15,8 @@ import { FireAuthService } from '@core/auth/fire-auth.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentDetailComponent implements OnInit {
-  cardCode!: string;
-  document$!: Observable<Document>;
+  cardCode: string;
+  document$: Observable<Document>;
   isAdmin$: Observable<boolean>;
 
   constructor(
@@ -30,39 +30,41 @@ export class DocumentDetailComponent implements OnInit {
   ) {
     this.cardCode = this.route.snapshot.params['cardCode'];
 
+    this.document$ = this.route.data.pipe(map((data) => data['document']));
+
     this.isAdmin$ = this.fireAuthService.isAdmin$;
   }
-  ngOnInit(): void {
-    if (this.cardCode) {
-      this.document$ = this.documentStoreService.getDocumentByCardCode(
-        this.cardCode
-      );
-    }
-  }
+  ngOnInit(): void {}
 
   deleteDocument(document: Document) {
+    const filename = document.cardCode;
+
     if (confirm('¿Confirmas eliminar oficio?') == true) {
       if (document.imageUrl) {
-        this.deleteFilesService.deleteFile(document.cardCode).subscribe();
+        this.deleteFilesService.deleteFile(filename).subscribe();
       }
 
       this.deleteDocumentsService
-        .deleteDocument(document.cardCode)
+        .deleteDocument(filename)
         .subscribe((docRef) => {
-          if (docRef === true) {
+          if (docRef == true) {
+            this.documentStoreService.deleteDocument(filename);
+
             this.router.navigate(['/home/oficios']);
           }
         });
     }
   }
 
-  cancelCard(id: string) {
+  cancelCard(document: Document) {
+    const id = document.cardCode;
+
     if (confirm('¿Confirmas cancelar tarjeta?') == true) {
       this.updateDocumentsService
         .updateDocument(id, { isCardCanceled: true })
         .subscribe((docRef) => {
-          if (docRef == this.cardCode) {
-            //do something
+          if (docRef == true) {
+            this.documentStoreService.updateDocument(id, document);
           }
         });
     }
