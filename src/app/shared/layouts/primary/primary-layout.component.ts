e48a0464694a61968b2d5a232e85133c330f975e
Mezame <mezamesoft@gmail.com>
2023-01-01 02:02:55 -0600

    Add log-out functionality

--- src/app/shared/layouts/primary/primary-layout.component.ts
+++ src/app/shared/layouts/primary/primary-layout.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { FireAuthService } from '@core/auth/fire-auth.service';
 import { map, Observable } from 'rxjs';
 
@@ -13,8 +14,20 @@ export class PrimaryLayoutComponent {
   isNotAdmin$: Observable<boolean>;
 
   @Input('toolbar-title') toolbarTitle?: string;
-  constructor(private fireAuthService: FireAuthService) {
+
+  constructor(
+    private fireAuthService: FireAuthService,
+    private router: Router
+  ) {
     this.isAdmin$ = this.fireAuthService.isAdmin$;
     this.isNotAdmin$ = this.isAdmin$.pipe(map((isAdmin) => !isAdmin));
+  }
+
+  logOut() {
+    this.fireAuthService.signOut().subscribe((res) => {
+      if (res == true) {
+        this.router.navigate(['/login']);
+      }
+    });
   }
 }
