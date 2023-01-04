167658551eb818ecab14e8d2af9311d64d23f466
Mezame <mezamesoft@gmail.com>
2023-01-04 15:54:36 -0600

    Refactor store functionality

--- src/app/features/reports/services/store/report-store.service.ts
+++ src/app/features/reports/services/store/report-store.service.ts
@@ -7,14 +7,38 @@ import { GetReportsService } from '../firestore/get-reports.service';
   providedIn: 'root',
 })
 export class ReportStoreService {
-  private readonly reports$ = new BehaviorSubject<Report[]>([]);
+  private readonly reports$: BehaviorSubject<Report[]>;
 
-  constructor(private getReportsService: GetReportsService) {}
+  private mockReports = [
+    {
+      date: '11/15/2022, 12:00:00 AM',
+      cardsStats: {
+        newRecord: 1,
+        replacement: 0,
+        change: 0,
+        cancel: 0,
+      },
+      cardCodesRange: ['P000000000', 'P000000000'],
+      sexStats: { male: 0, female: 1 },
+      metadata: {
+        id: 'VER-TUXPAN-noviembre-2022',
+      },
+    },
+  ] as Report[];
+
+  constructor(private getReportsService: GetReportsService) {
+    this.reports$ = new BehaviorSubject<Report[]>(this.mockReports);
+  }
 
   loadReports() {
-    this.getReportsService.getReports().subscribe((reports) => {
-      this.reports$.next(reports);
-    });
+    this.getReportsService
+      .getReports()
+      .pipe(map((reports) => (reports.length > 0 ? reports : null)))
+      .subscribe((reports) => {
+        if (reports) {
+          this.reports$.next(reports);
+        }
+      });
   }
 
   getReportsValue(): readonly Report[] {
@@ -26,7 +50,10 @@ export class ReportStoreService {
   getReports(): Observable<Report[]> {
     const reports = this.getReportsValue();
 
-    if (reports.length < 1) {
+    if (
+      reports.length < 1 ||
+      reports[0].metadata?.id == 'VER-TUXPAN-noviembre-2022'
+    ) {
       this.loadReports();
     }
 
