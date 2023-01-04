8f929234c2503987510a810526b89b779fc58984
Mezame <mezamesoft@gmail.com>
2023-01-04 15:54:27 -0600

    Refactor store functionality

--- src/app/features/documents/services/store/document-store.service.ts
+++ src/app/features/documents/services/store/document-store.service.ts
@@ -1,31 +1,58 @@
 import { Injectable } from '@angular/core';
 import { DateStore } from '@features/documents/date-store.interface';
 import { Document } from '@features/documents/document.interface';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, filter, map, mergeMap, Observable } from 'rxjs';
 import { GetDocumentsService } from '../firestore/get-documents.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentStoreService {
-  private readonly documents$ = new BehaviorSubject<Document[]>([]);
+  private readonly documents$: BehaviorSubject<Document[]>;
+  private readonly dateStore$: BehaviorSubject<DateStore>;
 
-  private readonly dateStore$ = new BehaviorSubject<DateStore>({
+  private mockDocuments = [
+    {
+      createDate: '11/9/2022, 2:45:49 PM',
+      operationCode: 'NUEVO REG',
+      cardCode: 'P000000000',
+      branchOffice: 'VER-TUXPAN',
+      reviewDocument: 'Valeria B. Alarcón',
+      makeCard: 'Valeria B. Alarcón',
+      fathersLastname: 'Vega',
+      mothersLastname: 'Hidalgo',
+      name: 'Ramona',
+      sex: 'Mujer',
+      birthdate: '10/31/1965, 4:16:41 PM',
+      birthplace: 'Veracruz',
+      curp: 'WUVJ302842THBYVQ00',
+      maritalStatus: 'Casado',
+    },
+  ] as Document[];
+  private mockdateStore = {
     months: {
-      ['']: {
-        numbers: [],
-        words: [],
+      ['2022']: {
+        numbers: ['11'],
+        words: ['noviembre'],
       },
     },
-    years: [],
-  });
+    years: ['2022'],
+  } as DateStore;
 
-  constructor(private getDocumentsService: GetDocumentsService) {}
+  constructor(private getDocumentsService: GetDocumentsService) {
+    this.documents$ = new BehaviorSubject<Document[]>(this.mockDocuments);
+    this.dateStore$ = new BehaviorSubject<DateStore>(this.mockdateStore);
+  }
 
   loadDocuments() {
-    this.getDocumentsService.getDocuments().subscribe((documents) => {
-      this.documents$.next(documents);
-    });
+    this.getDocumentsService
+      .getDocuments()
+      .pipe(map((documents) => (documents.length > 0 ? documents : null)))
+      .subscribe((documents) => {
+        if (documents) {
+          this.documents$.next(documents);
+        }
+      });
   }
 
   getDocumentsValue(): readonly Document[] {
@@ -37,7 +64,7 @@ export class DocumentStoreService {
   getDocuments(): Observable<Document[]> {
     const documents = this.getDocumentsValue();
 
-    if (documents?.length < 1) {
+    if (documents?.length < 1 || documents[0]?.cardCode == 'P000000000') {
       this.loadDocuments();
     }
 
@@ -97,15 +124,23 @@ export class DocumentStoreService {
   loadDocumentUtilsDateStore() {
     this.getDocumentsService
       .getDocumentUtilsDateStore()
+      .pipe(map((dateStore) => (dateStore.years?.length > 0 ? dateStore : null)))
       .subscribe((dateStore) => {
-        this.dateStore$.next(dateStore);
+        console.log(dateStore);
+        if (dateStore) {
+          this.dateStore$.next(dateStore);
+        }
       });
   }
 
   getDocumentUtilsDateStore(): Observable<DateStore> {
     const dateStore = this.dateStore$.getValue() as Readonly<DateStore>;
 
-    if (dateStore.years?.length < 1) {
+    if (
+      dateStore?.years.length < 1 ||
+      (dateStore?.years[0] == '2022' &&
+        dateStore?.months['2022'].numbers[0] == '11')
+    ) {
       this.loadDocumentUtilsDateStore();
     }
 
