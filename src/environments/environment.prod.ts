526c3a27c0001207960fc5386601ab53722cebd5
Mezame <mezamesoft@gmail.com>
2023-01-18 12:22:29 -0600

    Add production environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  useEmulators: true,
+  firebase: {
+    projectId: 'dif-inapam-app',
+    appId: '1:227451757890:web:16acec35ad07debe4e14db',
+    storageBucket: 'dif-inapam-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCSd3ra8xTxy0S2ciB5LdCkbNdJ5dUZfKY',
+    authDomain: 'dif-inapam-app.firebaseapp.com',
+    messagingSenderId: '227451757890',
+  },
+};
