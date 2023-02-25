761f69e74347c3c25101477ac00bcca30986c28b
Mezame <mezamesoft@gmail.com>
2023-02-24 22:10:34 -0600

    Add get-assistants test

--- src/app/features/assistants/services/firestore/get-assistants.service.spec.ts
+++ src/app/features/assistants/services/firestore/get-assistants.service.spec.ts
@@ -1,18 +1,81 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GetAssistantsService } from './get-assistants.service';
+import { environment } from '../../../../../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Assistant } from '@features/assistants/assistant.interface';
+import { of } from 'rxjs';
 
 describe('GetAssistantsService', () => {
-  let service: GetAssistantsService;
+  let getAssistantsService: jasmine.SpyObj<GetAssistantsService>;
 
   beforeEach(() => {
+    const getAssistantsServiceSpy = jasmine.createSpyObj(
+      'GetAssistantsService',
+      ['getAssistants']
+    );
+
     TestBed.configureTestingModule({
-      providers: [{ provide: GetAssistantsService, useValue: {} }],
+      imports: [
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideFirestore(() => getFirestore()),
+      ],
+      providers: [
+        { provide: GetAssistantsService, useValue: getAssistantsServiceSpy },
+      ],
     });
-    service = TestBed.inject(GetAssistantsService);
+
+    getAssistantsService = TestBed.inject(
+      GetAssistantsService
+    ) as jasmine.SpyObj<GetAssistantsService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(getAssistantsService).toBeTruthy();
+  });
+
+  describe('#getAssistants', () => {
+    let expectedAssistants: Assistant[];
+
+    beforeEach(() => {
+      expectedAssistants = [
+        {
+          name: 'Efrain López',
+          email: 'assistant1@difinapam.mx',
+          metadata: { id: '1', timestamp: '' },
+        },
+        {
+          name: 'Sara González',
+          email: 'assistant2@difinapam.mx',
+          metadata: { id: '2', timestamp: '' },
+        },
+      ];
+    });
+
+    it('should return at least one assistant', () => {
+      getAssistantsService.getAssistants.and.returnValue(
+        of(expectedAssistants)
+      );
+
+      getAssistantsService.getAssistants().subscribe({
+        next: (assistants) =>
+          expect(assistants.length)
+            .withContext('should return assistants')
+            .toBeGreaterThan(0),
+        error: fail,
+      });
+    });
+
+    it('should be OK returning no assistants', () => {
+      getAssistantsService.getAssistants.and.returnValue(of([]));
+
+      getAssistantsService.getAssistants().subscribe({
+        next: (assistants) =>
+          expect(assistants.length)
+            .withContext('should have empty assistants array')
+            .toEqual(0),
+        error: fail,
+      });
+    });
   });
 });
